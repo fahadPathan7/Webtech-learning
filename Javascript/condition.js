7df9c50ddda1ec7f38fb252b2c4396520e7b3abb
Fahad Pathan <fahadpathan56@gmail.com>
2023-07-27 00:08:07 +0600

    Javascript added

---
+++ Javascript/condition.js
@@ -0,0 +1,5 @@
+var num = 15;
+
+if (num > 100) console.log("The number is greater than 100");
+else if (num == 100) console.log("The number is 100");
+else console.log("The number is less than 100");
