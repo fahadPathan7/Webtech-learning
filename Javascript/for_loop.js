d152e4da29187be174f08e1bc2cf5d3e03171732
Fahad Pathan <fahadpathan56@gmail.com>
2023-07-27 00:23:12 +0600

    for loop and switch added

---
+++ Javascript/for_loop.js
@@ -0,0 +1,4 @@
+// printing the numbers from 0 to 9
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
---
+++ Javascript/switch.js
@@ -0,0 +1,21 @@
+var num = 7;
+
+switch(num) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("two");
+        break;
+    case 3:
+        console.log("three");
+        break;
+    case 4:
+        console.log("four");
+        break;
+    case 5:
+        console.log("five");
+        break;
+    default:
+        console.log("greater than five.");
+}
