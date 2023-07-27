bb0ad28ba5debc4714a663727600adda24d42454
Fahad Pathan <fahadpathan56@gmail.com>
2023-07-28 00:32:32 +0600

    JS: Learned Math object

---
+++ Javascript/math_object.js
@@ -0,0 +1,24 @@
+var val = Math.sqrt(16);
+console.log(val); // 4
+
+val = Math.abs(-5);
+console.log(val); // 5
+
+val = Math.pow(2, 3);
+console.log(val); // 8
+
+val = Math.random(); // from greater than 0 to less than 1
+console.log(val);
+
+val = Math.floor(2.4);
+console.log(val); // 2
+
+val = Math.ceil(2.4);
+console.log(val); // 3
+
+val = Math.max(2, 3, 1, 7); // unlimited
+console.log(val); // 7
+
+val = Math.min(2, 3, 1, 7); // unlimited
+console.log(val); // 1
+
