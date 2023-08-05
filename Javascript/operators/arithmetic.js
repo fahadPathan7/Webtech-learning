3b153a0a36dffdc20571be4a9dc0c3e6fa3d4d90
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-05 22:45:12 +0600

    JS: arithmetic operators

---
+++ Javascript/operators/arithmetic.js
@@ -0,0 +1,26 @@
+
+// addition
+var a = 10 + 20;
+
+// subtraction
+var b = 20 - 10;
+
+// multiplication
+var c = 10 * 20;
+
+// division
+var d = 20 / 10;
+
+// modulus
+var e = 20 % 10;
+
+// increment
+var f = 10;
+f++;
+
+// decrement
+var g = 10;
+g--;
+
+// exponential
+var h = 10 ** 2;
