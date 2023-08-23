924a102083d8975727f3bece9e494bdcfb36e824
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-23 20:29:13 +0600

    add: array function

---
+++ Javascript/ES6/function/script.js
@@ -0,0 +1,11 @@
+
+// ES6 Arrow Function
+// i can not call it before declaring it.
+// i can not use it as a constructor.
+
+const add = (x, y) => {
+    let sum = x + y;
+    console.log(sum);
+}
+
+add(2, 3); // 5
