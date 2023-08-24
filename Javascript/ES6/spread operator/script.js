b92fd64a41df9488ff44d238e18f268ec0a29332
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-24 23:57:25 +0600

    update: spread operator

--- Javascript/ES6/spread operator/script.js
+++ Javascript/ES6/spread operator/script.js
@@ -12,4 +12,7 @@ let numbers1 = [...numbers, 4, 5, 6]; // I can also use spread operator to creat
 console.log(numbers1); // output: [ 1, 2, 3, 4, 5, 6 ]
 
 
-// i can add spread operator in between array elements. it will spread array elements as arguments. 
+// i can add spread operator in between array elements. it will spread array elements as arguments.
+
+let numbers2 = [1, ...numbers, 4, 5, 6];
+console.log(numbers2); // output: [ 1, 1, 2, 3, 4, 5, 6, 4, 5, 6 ]  // 1 is repeated because i have added it in the beginning of array.
