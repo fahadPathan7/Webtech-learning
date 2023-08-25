2e9f5191df38200e1164593d053ee1155de24898
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-25 20:34:50 +0600

    update: arrow function
    1. use with object

--- ES6/function/script.js
+++ ES6/function/script.js
@@ -3,9 +3,34 @@
 // i can not call it before declaring it.
 // i can not use it as a constructor.
 
-const add = (x, y) => {
-    let sum = x + y;
-    console.log(sum);
+// const add = (x, y) => {
+//     let sum = x + y;
+//     console.log(sum);
+// }
+
+// add(2, 3); // 5
+
+
+
+
+// another demo with object
+
+const student = [
+    { name: 'Jony', age: 20 },
+    { name: 'Mony', age: 21 },
+    { name: 'Pony', age: 22 },
+    { name: 'Bony', age: 23 },
+];
+
+const studentName = () => {
+    // return student.filter((x) => {
+    //     return x.age > 20;
+    // }).map((y) => {
+    //     return y.name;
+    // });
+
+    // can be written as
+    return student.filter(x => x.age > 20).map(y => y.name);
 }
 
-add(2, 3); // 5
+console.log(studentName());
