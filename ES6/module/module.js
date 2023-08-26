e7e87de139ee114555a46cc64b3e4aee275bb4c8
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-26 23:13:55 +0600

    add: module

---
+++ ES6/module/module.js
@@ -0,0 +1,5 @@
+export let text = 'Hello World';
+
+export const changeText = (txt) => {
+    text = txt;
+}
---
+++ ES6/module/script.js
@@ -0,0 +1,6 @@
+import {text, changeText} from './module.js';
+
+// output can be seen in the browser console
+console.log(text); // Hello World
+changeText('testing module');
+console.log(text); // testing module
