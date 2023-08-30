f9596b017dc3ebbe7baaba52b468f27202645be0
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-30 12:06:32 +0600

    update: api calling
    1. update data
    2. update specific data
    3. delete data

--- ES6/api calling/xhr.js
+++ ES6/api calling/xhr.js
@@ -1,3 +1,6 @@
+// guide: https://jsonplaceholder.typicode.com/guide/
+
+
 console.clear();
 
 const makeRequest = (method, url, data) => {
@@ -6,10 +9,11 @@ const makeRequest = (method, url, data) => {
 
     xhr.open(method, url); // open method is used to open the connection
 
-    xhr.setRequestHeader('Content-Type', 'application/json'); // is used to set the header of the request
+    xhr.setRequestHeader('Content-Type', 'application/json'); // is used to set the header of the request. it is needed because we are sending the data in json format.
 
     // work of onload is to get the response from the server
     xhr.onload = () => {
+        console.log(xhr.status); // status code like 200, 404, 500. 200 means success
         console.log(JSON.parse(xhr.response));
     }
 
@@ -19,6 +23,7 @@ const makeRequest = (method, url, data) => {
     }
 
     xhr.send(JSON.stringify(data)); // send method is used to send the request to the server
+    // JSON.stringify is used to convert the data into json format.
 }
 
 // to get the data from the server
@@ -35,5 +40,31 @@ const sendData = () => {
     });
 }
 
+// update the data to the server
+// it will update the whole data. It will not update the specific data.
+const updateData = () => {
+    // given link should be specific
+    makeRequest('PUT', 'https://jsonplaceholder.typicode.com/posts/1', {
+        id: 1, // id is required to update the data
+        title: 'footer',
+        body: 'barrier',
+        userId: 1,
+    });
+}
 
-sendData();
+// to change the specific data
+const updateSpecificData = () => {
+    // given link should be specific
+    makeRequest('PATCH', 'https://jsonplaceholder.typicode.com/posts/1', {
+        title: 'changed title',
+        body: 'changed body',
+    });
+}
+
+// to delete the data
+const deleteData = () => {
+    // given link should be specific
+    makeRequest('DELETE', 'https://jsonplaceholder.typicode.com/posts/1');
+}
+
+deleteData();
