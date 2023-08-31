adb1f8b7217357f448b00ea45480ddf79dedf905
Fahad Pathan <fahadpathan56@gmail.com>
2023-08-31 23:12:02 +0600

    update: api calling using fetch
    1. async await

--- ES6/api calling/fetch/fetch.js
+++ ES6/api calling/fetch/fetch.js
@@ -111,11 +111,84 @@ const deleteData = () => {
         .catch(error => console.log(error));
 }
 
-getData();
-postData();
-putData();
-patchData();
-deleteData();
+// getData();
+// postData();
+// putData();
+// patchData();
+// deleteData();
 
 
 // async await
+const makeRequest = async (url, config) => {
+    const response = await fetch(url, config);
+    if (!response.ok) {
+        throw new Error("ERROR: ${response.status}");
+    }
+    return response.json();
+}
+
+const getData2 = () => {
+    makeRequest('https://jsonplaceholder.typicode.com/posts/1')
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+
+const postData2 = () => {
+    makeRequest('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My post',
+            body: 'This is my post',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+
+const putData2 = () => {
+    makeRequest('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: 1, // update will be done on this id
+            title: 'Updated My post',
+            body: 'Updated This is my post',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+
+const patchData2 = () => {
+    makeRequest('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            title: 'patched My post'
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+
+const deleteData2 = () => {
+    makeRequest('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+
+postData2();
+putData2();
+patchData2();
+deleteData2();
