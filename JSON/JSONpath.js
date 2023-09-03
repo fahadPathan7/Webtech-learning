ab192401f0ec48c8e559c15c9d0ff635a14b1e29
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-03 23:31:02 +0600

    add: modify JSON data

--- JSON/JSONpath.js
+++ JSON/JSONpath.js
@@ -7,3 +7,10 @@ console.log(data.students[0].name); // John
 console.log(data.students[0].age); // 30
 console.log(data.students[0].languages[0]); // English
 console.log(data.students[0].friends[0].name); // Peter
+
+
+// modify the data
+data.students[0].name = 'Jane'; // change name from John to Jane
+console.log(data.students[0].name); // Jane
+
+
